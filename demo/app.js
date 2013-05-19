daf6c56375274531c6cf9101fc7ea941e23c1017
Vincenzo Ferrari <wilk3ert@gmail.com>
2013-05-20 01:12:33 +0200

    fixed examples

--- demo/app.js
+++ demo/app.js
@@ -5,81 +5,57 @@ Ext.Loader.setConfig ({
 	}
 });
 
-Ext.require (['Ext.ux.Deferred', 'Ext.ux.DeferredManager']);
+Ext.require (['Ext.ux.Deferred']);
 
-/*function aSync () {
-	var promise = Ext.create ('Ext.ux.Promise') ,
-		params = {
-			url: 'my.json' ,
-			success: function (data) {
-				promise.resolve (data);
-			} ,
-			failure: function (data) {
-				promise.reject (data);
-			}
-		};
-	
-	Ext.Ajax.request (params);
-	
-	return promise;
-}*/
-
-var globalCounter = 0;
-
-function aSync1 () {
+function aSync1 (data) {
 	var deferred = Ext.create ('Ext.ux.Deferred') ,
-		counter = 0 ,
 		task = setInterval (function () {
-			counter++;
-			
-			if (counter == 20) {
-				console.log ('RESOLVING aSync1');
-				deferred.resolve (10);
-				clearInterval (task);
-			}
-		}, 200);
+			console.log ('RESOLVING aSync1 - ', data);
+			deferred.resolve (10); //d1
+			clearInterval (task);
+		}, 2000);
 	
-	return deferred.promise ();
+	return deferred;
 }
 
-function aSync2 () {
+function aSync2 (data) {
 	var deferred = Ext.create ('Ext.ux.Deferred') ,
-		counter = 0 ,
 		task = setInterval (function () {
-			counter++;
-			
-			if (counter == 10) {
-				console.log ('RESOLVING aSync2');
-				deferred.resolve (20);
-				clearInterval (task);
-			}
-		}, 100);
+			console.log ('RESOLVING aSync2 - ', data);
+			deferred.resolve (20); //d1.lastDfd -> d2
+			clearInterval (task);
+		}, 1000);
 	
-	return deferred.promise ();
+	return deferred;
 }
 
-function aSync3 () {
+function aSync3 (data) {
 	var deferred = Ext.create ('Ext.ux.Deferred') ,
-		counter = 0 ,
 		task = setInterval (function () {
-			counter++;
-			
-			if (counter == 30) {
-				console.log ('RESOLVING aSync3');
-				deferred.resolve (30);
-				clearInterval (task);
-			}
-		}, 300);
+			console.log ('RESOLVING aSync3 - ', data);
+			deferred.resolve (30); //d2.lastDfd -> d3
+			clearInterval (task);
+		}, 3000);
 	
-	return deferred.promise ();
+	return deferred;
 }
+
+var d1,d2,d3,d4;
 
 Ext.onReady (function () {
-	Ext.ux.DeferredManager
-		.when (aSync1 (), aSync2 (), aSync3 ())
-		.always (function (data1, data2, data3) {
-			console.log ('ALWAYS : ' + data1 + ' - ' + data2 + ' - ' + data3);
+	/*d1 = aSync1(0);
+	d2 = d1.then(function (data) {console.log('last d1.then: ', data); return 20;});
+	d3 = d2.then(function (data) {console.log('last d2.then: ', data); return 30;});
+	d4 = d3.then(function (data) {console.log('last d3.then: ', data); return 40;});*/
+	
+	Ext.ux.Deferred
+		.when (aSync1 (0), aSync2, aSync3 (20))
+		.then (function (data) {
+			console.log ('done ', data);
 		});
+		/*.always (function (data1, data2, data3) {
+			console.log ('ALWAYS : ' + data1 + ' - ' + data2 + ' - ' + data3);
+		});*/
 		/*.done (function (data) {
 			console.log ('DONE : ' + data);
 		})
